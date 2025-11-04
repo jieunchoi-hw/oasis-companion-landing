644746b2d7f2cea7545cf6c1747df2a2cd6b308c
choijieun <choijieun@hanwha.com>
2025-11-04 20:46:49 +0900

    copy light nowrap

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -13,7 +13,7 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <Icons.logo className="h-6 w-auto" />
           <Badge className="hidden lg:block md:block">Companion</Badge>
-          <p className="text-center text-sm leading-loose md:text-left">
+          <p className="text-center text-sm leading-loose md:text-left md:whitespace-nowrap text-neutral-600 dark:text-neutral-400">
             Copyright © 2025 OASIS Companion. All rights reserved.
           </p>
         </div>
